b36a3a6629bf36fd3e168be33e7f140107171e4d
Aravind Reddy <aravind.reddy@certifyglobal.com>
2021-03-15 12:41:11 -0400

    Auto-generated commit

--- src/app/components/common/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './access-token-timer/access-token-timer.component';
-export * from './error-dialog/error-dialog.component';
-export * from './object-viewer/object-viewer.component';
-export * from './resources-menu/resources-menu.component';
--- src/app/services/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Barrel for all the services in this folder
- */
-export * from './auth.service';
-export * from './client-app.service';
-export * from './fhir-server.service';
-export * from './global.service';
-export * from './helper.service';
-export * from './smart.service';
